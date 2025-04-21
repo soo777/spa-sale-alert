2227fef23caae174923345c67eada1a980573fb4
soo777 <ssm19944@gmail.com>
2025-04-21 10:02:30 +0900

    env: SPA 세일 알리미 프로젝트 생성

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>SPA Sale Mobile App</Text>
+    </View>
+  );
+}
---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>SPA Sale Web Admin</h1>;
+}
