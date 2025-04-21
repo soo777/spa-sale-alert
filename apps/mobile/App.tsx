111867642d45c971fdaa09ac902dfa04f06993e1
soo777 <ssm19944@gmail.com>
2025-04-21 11:25:49 +0900

    env(mobile): mobile 프로젝트 생성

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>SPA Sale Mobile App</Text>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
